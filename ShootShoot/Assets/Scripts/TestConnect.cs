52e363af4c6ea6d9895369c9dd1e4fea1ff06b27
Pierre-Lctx <pierre.lechatreux@hotmail.fr>
2021-09-03 17:13:09 +0200

    commit

---
+++ ShootShoot/Assets/Scripts/TestConnect.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+
+        print("Connecting to server.");
+        PhotonNetwork.GameVersion = ;
+        PhotonNetwork.ConnectUsingSettings();
+        
+    }
+
+    public void OnConnectedToMaster()
+    {
+        print("Connected to server.");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason " + cause.ToString());
+    }
+}
